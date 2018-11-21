f5c7cfa20929bb9ed295667dee84c6f5371922a1
liyuanlu <963187848@qq.com>
2018-11-21 16:43:06 +0800

    新增闹钟布局

--- app/src/main/java/com/mlly/xxalarm/fragment/MyFragment.java
+++ app/src/main/java/com/mlly/xxalarm/fragment/MyFragment.java
@@ -18,6 +18,6 @@ public class MyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_alarm,container,false);
+        return inflater.inflate(R.layout.item_alarm,container,false);
     }
 }
