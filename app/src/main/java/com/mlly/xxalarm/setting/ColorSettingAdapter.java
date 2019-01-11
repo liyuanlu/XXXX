2ad3218f0c7a5dc067ecdc6512d45012d1b40c62
liyuanlu <963187848@qq.com>
2019-01-11 16:15:10 +0800

    更新课更改主题色

--- app/src/main/java/com/mlly/xxalarm/setting/ColorSettingAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.mlly.xxalarm.setting;
-
-/**
- * Created by liyuanlu on 2019/1/11.
- */
-public class ColorSettingAdapter {
-}
--- app/src/main/java/com/mlly/xxalarm/setting/SettingActivity.java
+++ app/src/main/java/com/mlly/xxalarm/setting/SettingActivity.java
@@ -2,51 +2,87 @@ package com.mlly.xxalarm.setting;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
-import com.daobao.asus.dbbaseframe.mvpTest.TestContract;
 import com.mlly.xxalarm.R;
 
-public class SettingActivity extends AppCompatActivity{
+public class SettingActivity extends AppCompatActivity {
 
-    private Button button;
+    private ImageView mColorBlue;
+    private ImageView mColorPink;
+    private ImageView mColorGreen;
+    private ImageView mColorPurple;
+    private ImageView mColorDefault;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSPTheme();
         setContentView(R.layout.activity_setting);
-        button = (Button)findViewById(R.id.change_theme);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveTheme();
-            }
-        });
+        initView();
     }
 
     private void getSPTheme() {
-        SharedPreferences sp = getSharedPreferences("save_theme",Context.MODE_PRIVATE);
-        int theme = sp.getInt("bule",0);
+        SharedPreferences sp = getSharedPreferences("save_theme", Context.MODE_PRIVATE);
+        int theme = sp.getInt("bule", 0);
         Log.d("theme", "" + theme);
-        switch (theme){
-            case 1:setTheme(R.style.BlueTheme);
-            break;
-            default:break;
+        switch (theme) {
+            case 1:
+                setTheme(R.style.BlueTheme);
+                break;
+            default:
+                break;
         }
         setResult(100);
     }
 
-    private void saveTheme() {
-        SharedPreferences sp = getApplicationContext().getSharedPreferences("save_theme",Context.MODE_PRIVATE);
+    private void saveTheme(int color) {
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("save_theme", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putInt("bule",1);
+        editor.putInt("color", color);
         editor.apply();
-        Log.d("theme", "saveTheme: success");
         recreate();
+    }
+
+    private void initView() {
+        mColorBlue = (ImageView) findViewById(R.id.color_blue);
+        mColorBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTheme(1);
+            }
+        });
+        mColorPink = (ImageView) findViewById(R.id.color_pink);
+        mColorPink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTheme(2);
+            }
+        });
+        mColorGreen = (ImageView) findViewById(R.id.color_green);
+        mColorGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTheme(3);
+            }
+        });
+        mColorPurple = (ImageView) findViewById(R.id.color_purple);
+        mColorPurple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTheme(4);
+            }
+        });
+        mColorDefault = (ImageView) findViewById(R.id.color_default);
+        mColorDefault.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTheme(0);
+            }
+        });
     }
 }
